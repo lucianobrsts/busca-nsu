a8cd5137e626ff0e0680410eea8792340a93753c
lucianobrsts <lucianobrsts@yahoo.com.br>
2021-03-05 10:36:11 -0300

    Criada entidade NSU

---
+++ src/main/java/br/com/boavista/domain/Nsu.java
@@ -0,0 +1,57 @@
+package br.com.boavista.domain;
+
+public class Nsu {
+
+	private int codigo;
+	private String nome;
+
+	public int getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(int codigo) {
+		this.codigo = codigo;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + codigo;
+		result = prime * result + ((nome == null) ? 0 : nome.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Nsu other = (Nsu) obj;
+		if (codigo != other.codigo)
+			return false;
+		if (nome == null) {
+			if (other.nome != null)
+				return false;
+		} else if (!nome.equals(other.nome))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Nsu [codigo=" + codigo + ", nome=" + nome + "]";
+	}
+
+}
